66fd73b30522e0a57557dc0e6b38db3c37e52a00
Yibo Zhao <yibozhao2297@gmail.com>
2025-11-06 13:48:10 +0000

    Extract Decode pQTLs for mucin region
    decode pQTLs are b38

---
+++ extract_decode_mucin.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# SLURM directives:
+#SBATCH --job-name=extract_decode_pQTL
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=50G
+#SBATCH --time=12:00:00
+#SBATCH --export=NONE
+
+cd /data/gen1/reference/decode_pQTL
+
+> /scratch/gen1/yz735/updated_coloc/decode_pQTL_mucin.txt
+
+for file in *.txt.gz; do
+    echo "Processing $file..." >&2
+    zcat "$file" | awk '$1 == "chr11" && $2 >= 1012240  && $2 <= 1274371' >> /scratch/gen1/yz735/updated_coloc/decode_pQTL_mucin.txt
+done
+
+hostname
+date
