91d0623c448cda65650a89e239cc66732634cc93
Yibo Zhao <yibozhao2297@gmail.com>
2025-11-07 11:04:17 +0000

    Add file names for protein ID reference

--- extract_decode_mucin.sh
+++ extract_decode_mucin.sh
@@ -13,9 +13,14 @@ cd /data/gen1/reference/decode_pQTL
 
 > /scratch/gen1/yz735/updated_coloc/decode_pQTL_mucin.txt
 
-for file in *.txt.gz; do
+for file in *b37.txt.gz; do
     echo "Processing $file..." >&2
-    zcat "$file" | awk '$1 == "chr11" && $2 >= 1012240  && $2 <= 1274371' >> /scratch/gen1/yz735/updated_coloc/decode_pQTL_mucin.txt
+   
+for file in *b37.txt.gz; do
+    echo "Processing $file..." >&2
+    zcat "$file" | awk -v fname="$file" '$1 == "chr11" && $2 >= 1012240 && $2 <= 1274371 { print $0 "\t" fname }' >> /scratch/gen1/yz735/updated_coloc/decode_pQTL_mucin.txt
+done
+
 done
 
 hostname
