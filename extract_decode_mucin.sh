775588eb0b576c7ba93d68e22ae3c0cd19a464b4
Yibo Zhao <yibozhao2297@gmail.com>
2025-11-17 10:48:43 +0000

    Update extract_decode_mucin.sh

--- extract_decode_mucin.sh
+++ extract_decode_mucin.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=50G
-#SBATCH --time=12:00:00
+#SBATCH --time=72:00:00
 #SBATCH --export=NONE
 
 cd /data/gen1/reference/decode_pQTL
@@ -15,12 +15,7 @@ cd /data/gen1/reference/decode_pQTL
 
 for file in *b37.txt.gz; do
     echo "Processing $file..." >&2
-   
-for file in *b37.txt.gz; do
-    echo "Processing $file..." >&2
-    zcat "$file" | awk -v fname="$file" '$1 == "chr11" && $2 >= 1012240 && $2 <= 1274371 { print $0 "\t" fname }' >> /scratch/gen1/yz735/updated_coloc/decode_pQTL_mucin.txt
-done
-
+    zcat "$file" | awk -v fname="$file" '$1 == "chr11" && $2 >= 1012240 && $2 <= 1274371 { print $0 "\t" fname }'  >> /scratch/gen1/yz735/updated_coloc/decode_pQTL_mucin.txt
 done
 
 hostname
